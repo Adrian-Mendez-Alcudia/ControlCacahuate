6d44fffe84957e28b0a0d16bfd6eb049b506803d
Adrian-Mendez-Alcudia <98040217+Adrian-Mendez-Alcudia@users.noreply.github.com>
2025-12-07 20:05:39 -0600

    Configure Firebase hosting and environments
    Added Firebase hosting configuration and deployment script. Updated Angular build budgets and CLI analytics settings. Populated environment files with Firebase credentials for development and production. Adjusted package dependencies and versions for Angular and Firebase.

--- control-cacahuate/src/environments/environment.development.ts
+++ control-cacahuate/src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCAwZfYJrpa-IK6wuS-XOmloYMYgN36AdU',
+    authDomain: 'control-cacahuate-app.firebaseapp.com',
+    projectId: 'control-cacahuate-app',
+    storageBucket: 'control-cacahuate-app.firebasestorage.app',
+    messagingSenderId: '769659970104',
+    appId: '1:769659970104:web:3ff6436840404a2f6e7ccd',
+  },
+};
